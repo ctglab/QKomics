1debd7d0bfaba14e55b25adcc99fb9b8b017c84b
repettovaleria <valeria.repetto291@gmail.com>
2023-09-21 13:00:47 +0200

    Added func for kernel computation & data proc

---
+++ Kernels/src/Preprocessing.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+import argparse
+import os
+import pickle
+
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.preprocessing import MinMaxScaler
+
+
+
+
+
+
+
+def Sample(data_input,features,labels,samp_size,):
+    """
+    Split 
+    """
+    df_tot_sel=data_input.sample(samp_size,random_state=89)
+    y_train=df_tot_sel[labels]
+    X_train=df_tot_sel[features]
+    return X_train,y_train
+
+
+
+
+def Split_and_sample(df_tot_sel,features,labels,
+                     tr_sz=0.6,ts_sz=0.4,
+                     tr_size=70,ts_size=70):
+    """
+    Take dataframe split in test and train and sample balancing classes,returns X_train/test and y_train/test
+
+    """
+    #Extract data and Split train and test
+    X_train, X_test, y_train, y_test = train_test_split(df_tot_sel[features],df_tot_sel[labels],
+                                                        train_size=tr_sz,test_size=ts_sz,
+                                                        random_state=89,stratify=df_tot_sel[labels],balanced=True)
+            
+
+    print(X_train.shape)
+    ########BALANCED SAMPLING BY CLASS###################################
+    if balanced ==True:
+        X_train['Class']=y_train
+        g_tr = X_train.groupby('Class')
+        print("Train class dist:")
+        print(g_tr.size())
+        samp_size_tr=min(tr_size,g_tr.size().min())
+        X_train= pd.DataFrame(g_tr.apply(lambda x: x.sample(samp_size_tr,random_state=89)))
+        print(type(X_train))
+        y_train=X_train['Class'].droplevel(level='Class')
+        X_train=X_train[features].droplevel(level='Class')
+
+        #test
+        X_test['Class']=y_test
+        g_ts = X_test.groupby('Class')
+        print("Test class dist:")
+        print(g_ts.size())
+        samp_size_ts=min(ts_size,g_ts.size().min())
+        #X_test= pd.DataFrame(g_ts.apply(lambda x: x.sample(ts_size,random_state=89)))
+        X_test= pd.DataFrame(g_ts.apply(lambda x: x.sample(samp_size_ts,random_state=89)))
+        y_test=X_test['Class'].droplevel(level='Class')
+        X_test=X_test[features].droplevel(level='Class')
+        
+        #Transform in numpy obj
+        y_train= y_train.to_numpy()
+        y_test= y_test.to_numpy()
+
+    return X_train,y_train,X_test,y_test
+
--- Kernels/src/kernels_quantum.py
+++ Kernels/src/kernels_quantum.py
@@ -1,5 +1,5 @@
 #############SCRIPT DESCRIPTION########################################################
-# Quantum kernel for more qubits encoding metabric cna and exp pca features
+# Functions for Quantum kernel for Q-kernel computation on qiskit(0.42.1)
 #Case all points
 # Bandwidth opt
 #
@@ -14,3 +14,31 @@ from qiskit import BasicAer
 from qiskit.circuit.library import ZZFeatureMap, ZFeatureMap
 from qiskit.utils import QuantumInstance, algorithm_globals
 from qiskit_machine_learning.kernels import QuantumKernel
+
+
+def Compute_kernel(X_train,X_test,adhoc_kernel):
+    """
+    Compute kenel for given data and kernel type
+    X_train=numpy array of data (train)
+    X_test=numpy array of data(test )
+    adhoc_kernel= QuantumKernel obj 
+    """
+
+    qkernel=adhoc_kernel.evaluate(X_test,X_train)
+    return qkernel
+
+def Save_kernel(qkernel,dir,tag):
+    """
+    Save kernel matrix as pickle
+    qkernel=Numpy matrix 
+    dir=string with directory to save qkernel as pickle
+    tag=string with kernel specific tag
+    """
+    with open(dir+'qk_tot_{}.pickle'.format(round(tag,2)),'wb') as f:
+                        pickle.dump(qkernel, f)
+
+def Compute_and_save_kernel(X_train,X_test,adhoc_kernel,dir,tag):
+        """
+        """
+        qkernel=Compute_kernel(X_train,X_test,adhoc_kernel)
+        Save_kernel(qkernel,dir,tag)
